157a12a90fb82599cc34e7c3eae99573597df818
Jin <world515world@gmail.com>
2018-10-01 11:50:23 +0800

    fixed partners

--- web/apps/partners/admin.py
+++ web/apps/partners/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib import admin
 from .models import PartnersInformation
 # Register your models here.
-from django_summernote.admin import SummernoteModelAdmin
-
-# Apply summernote to all TextField in model.
 
 
-class AdminPartnersInformation(SummernoteModelAdmin):  # instead of ModelAdmin
-    summernote_fields = ('description',)
+class AdminPartnersInformation(admin.ModelAdmin):
     list_display = (
-        'title'
+        'title',
+        'description'
+    )
+    list_editable = (
+        'title',
+        'description'
     )
 
 
