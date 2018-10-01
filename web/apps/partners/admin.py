0230fb7001cd677d39d74064481e64cd7202ec5c
Jin <world515world@gmail.com>
2018-10-01 13:11:06 +0800

    q

--- web/apps/partners/admin.py
+++ web/apps/partners/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import PartnersInformation
 
 # Register your models here.
+
+
+class AdminPartnersInformation(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description'
+    )
+
+
+admin.site.register(PartnersInformation, AdminPartnersInformation)
--- web/apps/partners/views.py
+++ web/apps/partners/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import PartnersInformation
 
-# Create your views here.
+
+class PartnersView(generic.TemplateView):
+    template_name = 'partners.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        ps_infos = PartnersInformation.objects.order_by('pk')
+        context['partners_infos'] = ps_infos
+        return context
