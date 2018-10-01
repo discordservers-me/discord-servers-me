4a354912265600f17754b14b0b0017608af0f8fc
Jin <world515world@gmail.com>
2018-10-01 12:17:12 +0800

    c

---
+++ web/apps/partners/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class PartnersInformation(models.Model):
+    title = models.CharField(max_length=200, help_text='A title that does not show in the page. Only in the Admin interface.')
+    description = models.TextField(max_length=5000, blank=True, help_text='Edit as you want, or use Code View to edit in HTML.')
+
+    def __str__(self):
+        return self.title
--- web/config/urls.py
+++ web/config/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('u/', include('web.apps.users.urls')),
+    path('partners/', include('web.apps.partners.urls')),
     path('auth/', include('web.apps.auths.urls')),
     path('giveaway/', include('web.apps.giveaways.urls')),
     path('servers/', include('web.apps.servers.urls')),
