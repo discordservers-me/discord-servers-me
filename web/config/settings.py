6ca23bae1a1c64062a7900520949c02e22b25b9a
Jin <world515world@gmail.com>
2018-10-02 16:39:24 +0800

    aa

--- web/config/settings.py
+++ web/config/settings.py
@@ -141,8 +141,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Media (User uploaded) url and root
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/img/media')
-MEDIA_URL = 'static/img/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Discord Bot settings
 BOT_TOKEN = config('BOT_TOKEN')
