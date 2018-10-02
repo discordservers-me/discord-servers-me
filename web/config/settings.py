ed2a4cc67e182981c24a199c91f3876e919b308f
Jin <world515world@gmail.com>
2018-10-02 10:47:48 +0800

    qwe

--- web/config/settings.py
+++ web/config/settings.py
@@ -141,7 +141,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Media (User uploaded) url and root
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
