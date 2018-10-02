d14fea65914c2cae8dad98972632c0e348719717
Jin <world515world@gmail.com>
2018-10-02 11:21:59 +0800

    aa

--- web/config/settings.py
+++ web/config/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Media (User uploaded) url and root
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'partners')
 
 
 # Discord Bot settings
