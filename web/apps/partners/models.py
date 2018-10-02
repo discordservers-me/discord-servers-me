fc7699f4cd9570dc9161f7d741722f891b1f058c
Jin <world515world@gmail.com>
2018-10-02 12:22:08 +0800

    ss

--- web/apps/partners/models.py
+++ web/apps/partners/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 def upload_location(instance, filename):
     filebase, extension = filename.split(".")
-    return "%s.%s" %(filebase, extension)
+    return "%s.%s" % (filebase, extension)
 
     # PartnerModel = instance.__class__
     # new_id = PartnerModel.objects.order_by("id").last().id + 1
@@ -15,7 +15,7 @@ class PartnersInformation(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=5000, blank=True)
     url = models.CharField(max_length=256)
-    image = models.ImageField(upload_to=upload_location,
+    image = models.ImageField(upload_to=None,
                               null=True,
                               blank=True,
                               width_field="width_field",
