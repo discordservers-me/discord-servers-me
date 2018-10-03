70107c00a16861c6240ba0cb9a48a084c91922b9
An Tran <trantinan2512@gmail.com>
2018-10-03 09:35:12 +0700

    test

--- bot/launch.py
+++ bot/launch.py
@@ -42,30 +42,30 @@ async def on_ready():
     #     await bot.change_presence(game=discord.Game(name=presence))
 
 
-@bot.event
-async def on_guild_join(guild):
-    update_or_create_server(guild)
-    print(f'Joined guild: {guild.name}')
+# @bot.event
+# async def on_guild_join(guild):
+#     update_or_create_server(guild)
+#     print(f'Joined guild: {guild.name}')
 
 
-@bot.event
-async def on_guild_remove(guild):
-    server_id = guild.id
+# @bot.event
+# async def on_guild_remove(guild):
+#     server_id = guild.id
 
-    try:
-        server_obj = DiscordServer.objects.get(server_id=server_id)
-        # delete the guild (server) from database, and all related database
-        # like emojis
-        server_obj.delete()
-        print(f'Removed from guild: {guild.name}. Data deleted.')
-    except DiscordServer.DoesNotExist:
-        print(f'This server (ID: {server_id}) does not exist in Database.')
+#     try:
+#         server_obj = DiscordServer.objects.get(server_id=server_id)
+#         # delete the guild (server) from database, and all related database
+#         # like emojis
+#         server_obj.delete()
+#         print(f'Removed from guild: {guild.name}. Data deleted.')
+#     except DiscordServer.DoesNotExist:
+#         print(f'This server (ID: {server_id}) does not exist in Database.')
 
 
-@bot.event
-async def on_guild_update(guild_before, guild_after):
-    update_or_create_server(guild_after)
-    print(f'Guild [{guild_after.name}] updated')
+# @bot.event
+# async def on_guild_update(guild_before, guild_after):
+#     update_or_create_server(guild_after)
+#     print(f'Guild [{guild_after.name}] updated')
 
 
 # @bot.event
@@ -80,72 +80,72 @@ async def on_guild_update(guild_before, guild_after):
 #     print(f'Guild [{member.guild.name}]\'s member count decreased.')
 
 
-@bot.event
-async def on_server_emojis_update(before, after):
-    # try to get the server object from database
-    # return if not exist (can't sync without server object)
-    try:
-        if before:
-            server_obj = DiscordServer.objects.get(server_id=before[0].server.id)
-        else:
-            server_obj = DiscordServer.objects.get(server_id=after[0].server.id)
-    except DiscordServer.DoesNotExist:
-        print('DiscordServer does not exist.')
-        return
+# @bot.event
+# async def on_server_emojis_update(before, after):
+#     # try to get the server object from database
+#     # return if not exist (can't sync without server object)
+#     try:
+#         if before:
+#             server_obj = DiscordServer.objects.get(server_id=before[0].server.id)
+#         else:
+#             server_obj = DiscordServer.objects.get(server_id=after[0].server.id)
+#     except DiscordServer.DoesNotExist:
+#         print('DiscordServer does not exist.')
+#         return
 
-    # logic to decide if an emoji is added or deleted
-    deleted = []
-    added = []
-    for e in before:
-        if e not in after:
-            deleted.append(e)
+#     # logic to decide if an emoji is added or deleted
+#     deleted = []
+#     added = []
+#     for e in before:
+#         if e not in after:
+#             deleted.append(e)
 
-    for e in after:
-        if e not in before:
-            added.append(e)
+#     for e in after:
+#         if e not in before:
+#             added.append(e)
 
-    if deleted:
-        for e in deleted:
-            try:
-                db_emoji = DiscordEmoji.objects.get(emoji_id=e.id)
-                print(f'Emoji {db_emoji.emoji} deleted.')
-                db_emoji.delete()
-            except DiscordEmoji.DoesNotExist:
-                # not exists, no more actions needed
-                pass
+#     if deleted:
+#         for e in deleted:
+#             try:
+#                 db_emoji = DiscordEmoji.objects.get(emoji_id=e.id)
+#                 print(f'Emoji {db_emoji.emoji} deleted.')
+#                 db_emoji.delete()
+#             except DiscordEmoji.DoesNotExist:
+#                 # not exists, no more actions needed
+#                 pass
 
-    elif added:
-        for e in added:
-            server_emoji, created = DiscordEmoji.objects.update_or_create(
-                server=server_obj,
-                emoji_id=e.id,
-                defaults={
-                    'emoji': e.name,
-                    'emoji_url': e.url
-                }
-            )
-            if created:
-                print(f'Emoji {server_emoji.emoji} stored.')
-    # updated existing emoji (change name)
-    else:
-        before_as_name = []
-        after_as_name = []
-        for e in before:
-            before_as_name.append(e.name)
+#     elif added:
+#         for e in added:
+#             server_emoji, created = DiscordEmoji.objects.update_or_create(
+#                 server=server_obj,
+#                 emoji_id=e.id,
+#                 defaults={
+#                     'emoji': e.name,
+#                     'emoji_url': e.url
+#                 }
+#             )
+#             if created:
+#                 print(f'Emoji {server_emoji.emoji} stored.')
+#     # updated existing emoji (change name)
+#     else:
+#         before_as_name = []
+#         after_as_name = []
+#         for e in before:
+#             before_as_name.append(e.name)
 
-        for e in after:
-            after_as_name.append(e.name)
+#         for e in after:
+#             after_as_name.append(e.name)
 
-        after_update = list(set(after_as_name) - set(before_as_name))[0]
-        before_update = list(set(before_as_name) - set(after_as_name))[0]
-        try:
-            updated_emoji = DiscordEmoji.objects.get(server=server_obj, emoji=before_update)
-        except DiscordEmoji.DoesNotExist:
-            print('DiscordEmoji does not exist.')
-            return
-        updated_emoji.emoji = after_update
-        updated_emoji.save()
-        print(f'Updated emoji "{updated_emoji.emoji}" from "{before_update}"')
+#         after_update = list(set(after_as_name) - set(before_as_name))[0]
+#         before_update = list(set(before_as_name) - set(after_as_name))[0]
+#         try:
+#             updated_emoji = DiscordEmoji.objects.get(server=server_obj, emoji=before_update)
+#         except DiscordEmoji.DoesNotExist:
+#             print('DiscordEmoji does not exist.')
+#             return
+#         updated_emoji.emoji = after_update
+#         updated_emoji.save()
+#         print(f'Updated emoji "{updated_emoji.emoji}" from "{before_update}"')
 
 
 def update_or_create_server(guild):
@@ -226,7 +226,6 @@ async def bump_premium_servers():
         now = django_timezone.now()
         tier1 = DiscordServer.objects.filter(premium_tier=1).order_by('bumped_at')[:4]
         tier2 = DiscordServer.objects.filter(premium_tier=2).order_by('bumped_at')[:7]
-        print(tier1, tier2)
         for server in (list(tier1) + list(tier2)):
             premium_tier = server.check_premium()
             if premium_tier == 0:
