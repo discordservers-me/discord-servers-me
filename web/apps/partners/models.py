bf5346513e8bf3566925612609dbab3147805592
Jin <world515world@gmail.com>
2018-10-09 10:19:29 +0800

    z

--- web/apps/partners/models.py
+++ web/apps/partners/models.py
@@ -12,7 +12,7 @@ class PartnersInformation(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=5000, blank=True)
     url = models.CharField(max_length=256)
-    image = models.CharField(max_length=250, blank=True)
+    image_url = models.CharField(max_length=250, blank=True)
 
     def __str__(self):
         return self.title
