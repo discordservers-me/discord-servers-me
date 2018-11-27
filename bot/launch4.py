2943774727147d6dfd07a23d429f962d59959beb
unknown <mittalaryan99@gmail.com>
2018-11-27 14:18:54 -0600

    Added two launch files and modified run.py

---
+++ bot/launch4.py
@@ -0,0 +1,16 @@
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.config.settings')
+django.setup()
+from django.conf import settings  # noqa
+from bot import tasks  # noqa
+from bot.bot import DiscordServersShardedClient  # noqa
+
+bot = DiscordServersShardedClient(shard_ids=list(range(9, 12)), shard_count=6, max_messages=101)
+
+# if not settings.DEBUG:
+#     bot.loop.create_task(tasks.bump_premium_servers(bot))
+#     bot.loop.create_task(tasks.check_changed_manager(bot))
+#     bot.loop.create_task(tasks.update_servers_info(bot))
+
+bot.run(settings.BOT_TOKEN)
