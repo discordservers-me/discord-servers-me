ab7ba9a246d9221b5ade8e18db0bd922678763be
unknown <mittalaryan99@gmail.com>
2018-12-01 14:04:55 -0600

    fixing bot

--- run.py
+++ run.py
@@ -9,12 +9,6 @@ if __name__ == "__main__":
         elif bot == '2':
             from bot import launch2 as bot_launch
             bot_launch
-        elif bot == '3':
-            from bot import launch3 as bot_launch
-            bot_launch
-        elif bot == '4':
-            from bot import launch4 as bot_launch
-            bot_launch
         else:
             print('Specify a bot to run (1/2)')
     except IndexError:
