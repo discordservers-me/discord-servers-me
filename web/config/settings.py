6f80e3564174a95b1d460f66e4fdadc8fe4aa963
unknown <mittalaryan99@gmail.com>
2018-12-11 00:32:33 -0600

    testing bot optimization

--- web/config/settings.py
+++ web/config/settings.py
@@ -148,8 +148,8 @@ BOT_TOKEN = config('BOT_TOKEN')
 BOT_ID = config('BOT_ID')
 BOT_SECRET = config('BOT_SECRET')
 BUMP_DURATION = config('BUMP_DURATION', default=360, cast=int)
-UPDATE_MANAGERS_DURATION = config('UPDATE_MANAGERS_DURATION', default=600, cast=int)
-UPDATE_SERVERS_INFO = config('UPDATE_SERVERS_INFO', default=360, cast=int)
+UPDATE_MANAGERS_DURATION = config('UPDATE_MANAGERS_DURATION', default=1200, cast=int)
+UPDATE_SERVERS_INFO = config('UPDATE_SERVERS_INFO', default=600, cast=int)
 
 
 DISCORD_REDIRECT_URI = 'auth:discord_callback'
