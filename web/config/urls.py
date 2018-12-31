fa7a3856e2a2995d89b6150fc5f61c2a9f5af0ca
hades-99 <aryanm2@illinois.edu>
2018-12-31 12:57:33 +0530

    testing dynamic sitemap init

--- web/config/urls.py
+++ web/config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.views.generic import RedirectView, TemplateView
+from django.contrib.sitemaps.views import sitemap
 from web.apps.core.views import HomeView
 from . import settings
 
@@ -20,7 +21,8 @@ urlpatterns = [
     path('paypal/', include('paypal.standard.ipn.urls')),
     path('summernote/', include('django_summernote.urls')),
     path(settings.DISCORD_TAIL_URL, RedirectView.as_view(url=settings.DISCORD_INVITE_LINK), name='discord_server'),
-    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml")),
+    # path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml")),
+
     path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"))
 
 ]
